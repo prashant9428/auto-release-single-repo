df40ddc1efccdb451b401be6691cce14ae22deb9
prashant9428 <pathakprashant841@gmail.com>
2022-05-20 18:25:41 +0530

    fix: added semantic

---
+++ release.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "branches": ['+([0-9])?(.{+([0-9]),x}).x', 'development', {name: 'master', prerelease: true}],
+    "repositoryUrl": "https://github.com/prashant9428/auto-release-single-repo",
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github",
+        [
+            "@semantic-release/changelog",
+            {
+              "changelogFile": "docs/CHANGELOG.md"
+            }
+          ],
+          [
+            "@semantic-release/git",
+            {
+              "assets": ["docs/CHANGELOG.md"]
+            }
+          ]]
+
+}
