8d2a309d78e96a20a372c6158c3c8b906b19eef4
prashant pathak <pathak.prashant@byjus.com>
2022-06-16 08:42:08 +0530

    feat: updated user setting

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ function config({ dotenvPath = "" } = {}) {
      
      
      
+     
         
         const jsonFromEnv = getParsedEnv(dotenvPath);
         const { isKeyFound, notFoundAttr } = isRequiredKeyPresent(jsonFromEnv);
