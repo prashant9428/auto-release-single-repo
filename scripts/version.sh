5d706ee19905cf2bd3edf67f0d0f21654b86a651
prashant pathak <pathakprashant841@gmail.com>
2026-09-02 23:50:03 +0530

    Update GitHub access token in version.sh

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,13 +2,13 @@
   # DRAFT="false"
   # PRE="true"
   # BRANCH="development"
-  # GITHUB_ACCESS_TOKEN="token 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"
+  # GITHUB_ACCESS_TOKEN="token REDACTED_ROTATED_2026"
   # GENERATE_RELEASE_NOTES:"true"
   # REPO_OWNER="prashant9428"
   # REPO_NAME="test-package"
 
   # API_JSON=$(printf '{"tag_name": "%s","target_commitish": "%s","name": "%s","draft": %s,"prerelease": %s, "generate_release_notes": %s}' "$NODE_VERSION" "$BRANCH" "$NODE_VERSION"  "$DRAFT" "$PRE" "$GENERATE_RELEASE_NOTES")
-  # API_RESPONSE_STATUS=$(curl --data "$API_JSON" --header 'Accept: application/vnd.github.v3+json' --header 'Authorization: token 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'  -s -i https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases)
+  # API_RESPONSE_STATUS=$(curl --data "$API_JSON" --header 'Accept: application/vnd.github.v3+json' --header 'Authorization: token REDACTED_ROTATED_2026'  -s -i https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases)
   # echo "$API_RESPONSE_STATUS"
 
 PACKAGE_CHANGE=$(lerna changed  --ndjson)
